5dbe28f5ec030aa1e58dcdbfdf418d265be955c2
kyra.jacox <kyra.jacox@CLC-LAB209-15.TCSD.tooelesd.org>
2018-09-04 13:20:55 -0600

    First commit

---
+++ src/helloWorldofdoom/Hello_world.java
@@ -0,0 +1,25 @@
+/**
+ *hello world program prints hello world in console 
+ */
+package helloWorldofdoom;
+
+/**
+ * @author kyra.jacox
+ * 9-18-2018
+ * hello world
+ */
+public class Hello_world {
+
+	
+
+		
+	
+
+	
+	public static void main(String[] args) {
+		 System.out.println("Hello World");
+		
+		
+	}
+
+}
